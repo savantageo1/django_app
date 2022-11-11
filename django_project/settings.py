83c8be858b8fc77faab188269ad09b710fbd5d03
rid <ridwan.olatilewa@gmail.com>
2022-11-10 23:49:57 -0500

    adding again

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
